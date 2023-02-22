abe868e47184e724bac50e63eb8bd85c08e9414d
ramrodwal <55842803+ramrodwal@users.noreply.github.com>
2023-02-22 11:49:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Vartika moti aunty!"); 
+        System.out.println("Hello, Vartika moti!!"); 
     }
 }
